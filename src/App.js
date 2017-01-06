68b4e41da07a58ff4586ced2bc10f61f2451bc4a
郑俊鑫 <zhengjunxin@wepiao.com>
2017-01-06 16:47:18 +0800

    for fun

--- src/App.js
+++ src/App.js
@@ -3,19 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        time: 50 * 60,
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            const {time} = this.state
+
+            if (time > 1) {
+                this.setState({
+                    time: time - 1,
+                })
+            }
+            else {
+                clearInterval(this.timer)
+            }
+
+        }, 1000)
+    }
+
+    formatTime(time) {
+        const minutes = this.prefix(~~(time / 60))
+        const seconds = this.prefix(time % 60)
+
+        return {minutes, seconds}
+    }
+
+    prefix(num) {
+        return num > 10 ? num + '' : '0' + num
+    }
+
+    render() {
+        const {minutes, seconds} = this.formatTime(this.state.time)
+
+        return (
+            <div className="Time">
+                {`${minutes}:${seconds}`}
+            </div>
+        )
+    }
 }
 
 export default App;
